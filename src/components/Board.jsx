2611a089f43a5a37c9f30bb9ca8bd2eb66fb611a
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2022-12-08 17:57:44 +0300

    chore: add immutability, functional components, priority

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,12 +3,29 @@ import Square from "./Square";
 
 
 class Board extends React.Component {
-    renderSquare(props) {
-        return <Square value={props}/>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares: squares, xIsNext: !this.state.xIsNext});
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
@@ -32,5 +49,26 @@ class Board extends React.Component {
         );
     }
 }
+//                                          ОПРЕДЕЛЕНИЕ ПОБЕДИТЕЛЯ
+
+// function calculateWinner(squares) {
+//     const lines = [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6],
+//     ];
+//     for (let i = 0; i < lines.length; i++) {
+//         const [a, b, c] = lines[i];
+//         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//             return squares[a];
+//         }
+//     }
+//     return null;
+// }
 
 export default Board;
